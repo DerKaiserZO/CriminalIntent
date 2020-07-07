b032c5fd51c40f7c015b5c362ae664193ae4c45c
zo <ibrahima.dansokho@numherit.com>
2020-07-07 16:29:22 +0000

    Initial commit

---
+++ app/src/main/java/com/example/criminalintent/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.criminalintent;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+
+public class MainActivity extends SingleFragmentActivity {
+
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeFragment();
+    }
+}
